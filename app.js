6f3fb12406dc73c1ba45ed5389cde3369ad10be0
jamesbrentlingeriv-spec <jamesbrentlingeriv@gmail.com>
2026-06-21 14:14:21 -0400

    Add and compress Ray-Ban Clubmaster eyeglasses model, and add it to the catalog

--- app.js
+++ app.js
@@ -1372,6 +1372,22 @@ document.addEventListener('DOMContentLoaded', () => {
         scaleY: 1.0,
         scaleZ: 1.0
       }
+    },
+    "rb5154-clubmaster": {
+      name: "Clubmaster",
+      fileUrl: "eyeglasses/Ray-Ban/rb5154_clubmaster.glb",
+      transforms: {
+        posX: 0.0,
+        posY: 0.38,
+        posZ: 0.145,
+        rotX: 0.0,
+        rotY: 180.0,
+        rotZ: 0.0,
+        scale: 0.95,
+        scaleX: 1.0,
+        scaleY: 1.0,
+        scaleZ: 1.0
+      }
     }
   };
 
