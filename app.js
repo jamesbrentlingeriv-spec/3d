0adeb691b9d9b67f07735e8de5e568a435f18c8d
jamesbrentlingeriv-spec <jamesbrentlingeriv@gmail.com>
2026-06-26 03:28:01 -0400

    eco

--- app.js
+++ app.js
@@ -2373,6 +2373,40 @@ document.addEventListener('DOMContentLoaded', () => {
         scaleZ: 1.0
       }
     },
+    "eco-alta": {
+      name: "Alta",
+      fileUrl: "eyeglasses/ECO Eyewear/Alta.glb",
+      cliponUrl: "eyeglasses/ECO Eyewear/clipon/alta.png",
+      transforms: {
+        posX: 0.0,
+        posY: 0.38,
+        posZ: 0.145,
+        rotX: 0.0,
+        rotY: 180.0,
+        rotZ: 0.0,
+        scale: 0.95,
+        scaleX: 1.0,
+        scaleY: 1.0,
+        scaleZ: 1.0
+      }
+    },
+    "eco-mist": {
+      name: "Mist",
+      fileUrl: "eyeglasses/ECO Eyewear/Mist.glb",
+      cliponUrl: "eyeglasses/ECO Eyewear/clipon/Mist.png",
+      transforms: {
+        posX: 0.0,
+        posY: 0.38,
+        posZ: 0.145,
+        rotX: 0.0,
+        rotY: 180.0,
+        rotZ: 0.0,
+        scale: 0.95,
+        scaleX: 1.0,
+        scaleY: 1.0,
+        scaleZ: 1.0
+      }
+    },
     "fashiontabulous-10x238": {
       name: "10x238",
       fileUrl: "eyeglasses/Fashiontabulous/10x238.glb",
